2d9259c78da7f90b4284ff9ee790e1726a76d323
HLVA <hieuliem2005@gmail.com>
2026-07-26 08:53:51 -0400

    style: remove dark background from TetTrungThu section

--- src/components/sections/TetTrungThu.tsx
+++ src/components/sections/TetTrungThu.tsx
@@ -1,6 +1,6 @@
 export function TetTrungThu() {
   return (
-    <section id="TetTrungThu" className="flex justify-center items-center py-8 px-4 bg-[#0d1b4b]">
+    <section id="TetTrungThu" className="flex justify-center items-center py-8 px-4">
       <img
         src="/tet-trung-thu-2026.png"
         alt="Tết Trung Thu 2026 – Miễn Phí Vào Cửa, 20 Tháng 9, 4PM–9PM tại Giáo Xứ Các Thánh Tử Đạo Việt Nam"
